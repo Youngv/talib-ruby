ef9b09a791686bb6406d15c8cef6ceb662fe30be
Victor Yang <victor@rt4u.bid>
2025-01-17 19:01:39 +0800

    Support both "ta_lib" and "ta-lib" naming conventions

--- ext/talib/extconf.rb
+++ ext/talib/extconf.rb
@@ -2,5 +2,6 @@ require 'mkmf'
 
 dir_config("talib")
 
-have_library("ta_lib", "TA_Initialize")
+have_library("ta_lib", "TA_Initialize") || have_library("ta-lib", "TA_Initialize")
+
 create_makefile("talib")
